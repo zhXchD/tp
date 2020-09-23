a16868c00c44f959518e3e8e6e2511de2657b0c4
joshualiangxy <joshualiang.xy@gmail.com>
2020-09-23 15:03:21 +0800

    Add JournalStorage interface

---
+++ src/main/java/seedu/address/storage/JournalStorage.java
@@ -0,0 +1,41 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.ReadOnlyJournal;
+
+public interface JournalStorage {
+    /**
+     * Returns the file path of the journal data file.
+     */
+    Path getJournalFilePath();
+
+    /**
+     * Returns Journal data as a {@link ReadOnlyJournal}.
+     *   Returns {@code Optional.empty()} if storage file is not found.
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException if there was any problem when reading from the storage.
+     */
+    Optional<ReadOnlyJournal> readJournal() throws DataConversionException, IOException;
+
+    /**
+     * @see #getJournalFilePath()
+     */
+    Optional<ReadOnlyJournal> readJournal(Path filePath) throws DataConversionException, IOException;
+
+    /**
+     * Saves the given {@link ReadOnlyJournal} to the storage.
+     * @param journal cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveJournal(ReadOnlyJournal journal) throws IOException;
+
+    /**
+     * @see #saveJournal(ReadOnlyJournal)
+     */
+    void saveJournal(ReadOnlyJournal journal, Path filePath) throws IOException;
+
+}
