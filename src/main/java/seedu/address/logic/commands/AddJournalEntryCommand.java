7e072d161d94d17e66f762fefb32e2cdfc007b97
Zhu Xiaochen <zhuxiaochen1005@gmail.com>
2020-09-25 09:45:29 +0800

    Update checkstyle

--- src/main/java/seedu/address/logic/commands/AddJournalEntryCommand.java
+++ src/main/java/seedu/address/logic/commands/AddJournalEntryCommand.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE_AND_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 
 
 import seedu.address.logic.commands.exceptions.CommandException;
--- src/main/java/seedu/address/logic/parser/AddJournalEntryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddJournalEntryCommandParser.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE_AND_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.stream.Stream;
 
