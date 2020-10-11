6a86853299d27d9849401d6fe20c17aa341c988a
Zhu Xiaochen <zhuxiaochen1005@gmail.com>
2020-10-11 17:42:12 +0800

    Add DeleteJournalEntryCommand

---
+++ src/main/java/seedu/address/logic/commands/DeleteJournalEntryCommand.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.journal.Entry;
+
+/**
+ * Deletes a person identified using it's displayed index from the address book.
+ */
+public class DeleteJournalEntryCommand extends Command {
+
+    public static final String COMMAND_WORD = "deletej";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the entry identified by the index number used in the "
+            + "displayed entries list.\nParameters: INDEX (must be a positive "
+            + "integer)\nExample: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_ENTRY_SUCCESS =
+            "Deleted Entry: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteJournalEntryCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Entry> lastShownList = model.getFilteredEntryList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(
+                    Messages.MESSAGE_INVALID_ENTRY_DISPLAYED_INDEX);
+        }
+
+        Entry entryToDelete = lastShownList.get(targetIndex.getZeroBased());
+        // TODO: model.deleteEntry(entryToDelete);
+        return new CommandResult(
+                String.format(MESSAGE_DELETE_ENTRY_SUCCESS, entryToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this || (other instanceof DeleteJournalEntryCommand
+                && ((DeleteJournalEntryCommand) other).targetIndex.equals(targetIndex));
+    }
+}
