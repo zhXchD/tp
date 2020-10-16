397fc745f0ecfa870b2c0881334bf7defeed28b3
Lingy12 <russellgy1229@gmail.com>
2020-10-16 12:59:33 +0800

    Fix import statement

--- src/test/java/seedu/address/model/journal/EntryTest.java
+++ src/test/java/seedu/address/model/journal/EntryTest.java
@@ -1,6 +1,9 @@
 package seedu.address.model.journal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalEntries.TEST_ENTRY_DEFAULT;
 import static seedu.address.testutil.TypicalPersons.ALICE;
