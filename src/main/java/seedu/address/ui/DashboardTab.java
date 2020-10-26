0ab824920469338f56f8d7940d028ba039a3c710
Zhu Xiaochen <zhuxiaochen1005@gmail.com>
2020-10-27 02:42:50 +0800

    Adjust dashboard text

--- src/main/java/seedu/address/ui/DashboardTab.java
+++ src/main/java/seedu/address/ui/DashboardTab.java
@@ -1,17 +1,13 @@
 package seedu.address.ui;
 
-import javafx.beans.Observable;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import seedu.address.model.journal.Entry;
 import seedu.address.model.person.Person;
 
-import java.util.Stack;
 
 public class DashboardTab extends UiPart<Region> {
     private static final String FXML = "DashboardTab.fxml";
@@ -37,7 +33,7 @@ public class DashboardTab extends UiPart<Region> {
         super(FXML);
         this.title.setText("Hello there, welcome to IntelliJournal!");
         this.message.setText("IntelliJournal is here to helped you with your contacts and events with them!");
-        this.guide.setText("Type Ksome commands to start. Have no idea? Try typing \"help\"!");
+        this.guide.setText("Type some commands to start. Have no idea? Try typing \"help\"!");
         this.recentPersonList = new PersonListPanel(recent);
         this.frequentPersonList = new PersonListPanel(frequent);
         this.recentPersonListPanelPlaceholder.getChildren().add(recentPersonList.getRoot());
