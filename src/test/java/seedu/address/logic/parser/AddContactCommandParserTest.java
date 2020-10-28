891bea6b2a6cc4d01681425d4f7f7962ebaf4069
jazerler <lerjazer@gmail.com>
2020-10-29 06:15:32 +0800

    Remove deprecated test cases

--- src/test/java/seedu/address/logic/parser/AddContactCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddContactCommandParserTest.java
@@ -166,26 +166,6 @@ public class AddContactCommandParserTest {
                     expectedMessage
             );
 
-            // missing phone prefix
-            // assertParseFailure(parser,
-            //        NAME_DESC_BOB + VALID_PHONE_BOB + EMAIL_DESC_BOB
-            //                + ADDRESS_DESC_BOB,
-            //        expectedMessage
-            // );
-
-            // missing email prefix
-            // assertParseFailure(parser,
-            //        NAME_DESC_BOB + PHONE_DESC_BOB + VALID_EMAIL_BOB
-            //                + ADDRESS_DESC_BOB,
-            //        expectedMessage
-            // );
-
-            // missing address prefix
-            // assertParseFailure(parser,
-            //        NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-            //                + VALID_ADDRESS_BOB,
-            //        expectedMessage
-            // );
         }
 
         @Test
