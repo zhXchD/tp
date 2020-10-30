7c38c8bdfeba5dcd0ad0cd6b9e44f0a0f49d966b
Zhu Xiaochen <zhuxiaochen1005@gmail.com>
2020-10-30 12:53:05 +0800

    Edit person does not need to update the filtered list

--- src/main/java/seedu/address/logic/commands/EditContactCommand.java
+++ src/main/java/seedu/address/logic/commands/EditContactCommand.java
@@ -90,7 +90,7 @@ public class EditContactCommand extends Command {
         }
 
         model.setPerson(personToEdit, editedPerson);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+//        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(
                 String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedPerson));
     }
