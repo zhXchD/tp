1330368789fa67fb27af1b3a2434495fcb321dd3
jazerler <lerjazer@gmail.com>
2020-11-04 06:12:23 +0800

    AddAdd methods to check for same names in addressbook

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -38,6 +38,15 @@ public class UniquePersonList implements Iterable<Person> {
     }
 
     /**
+     * Returns true if the list contains a person with the same name as the
+     * given argument.
+     */
+    public boolean containsName(Person toCheck) {
+        requireNonNull(toCheck);
+        return internalList.stream().anyMatch(toCheck::hasSameName);
+    }
+
+    /**
      * Adds a person to the list.
      * The person must not already exist in the list.
      */
