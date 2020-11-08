03cbcab5c82e0228561db971100590e51f20b0c6
joshualiangxy <joshualiang.xy@gmail.com>
2020-11-09 00:23:02 +0800

    Fix naming of boolean values

--- src/main/java/seedu/address/logic/commands/EditContactCommand.java
+++ src/main/java/seedu/address/logic/commands/EditContactCommand.java
@@ -64,12 +64,14 @@ public class EditContactCommand extends Command {
      * @param index of the person in the filtered person list to edit
      * @param editPersonDescriptor details to edit the person with
      */
-    public EditContactCommand(Index index, EditPersonDescriptor editPersonDescriptor) {
+    public EditContactCommand(
+            Index index, EditPersonDescriptor editPersonDescriptor) {
         requireNonNull(index);
         requireNonNull(editPersonDescriptor);
 
         this.index = index;
-        this.editPersonDescriptor = new EditPersonDescriptor(editPersonDescriptor);
+        this.editPersonDescriptor =
+                new EditPersonDescriptor(editPersonDescriptor);
     }
 
     public static String getMessageUsage(String commandWord) {
